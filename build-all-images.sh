83032ad4c84e62407e3147078be263fecfae5ee5
Skazza <marianoscazzariello@gmail.com>
2017-06-30 14:10:59 +0200

    Fix Secondo Progetto

--- build-all-images.sh
+++ build-all-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build --rm -t swarm.inf.uniroma3.it:5000/servizio-9009/eureka ./eureka
-docker build --rm -t swarm.inf.uniroma3.it:5000/servizio-9009/cities ./cities
-docker build --rm -t swarm.inf.uniroma3.it:5000/servizio-9009/ratings ./ratings
-docker build --rm -t swarm.inf.uniroma3.it:5000/servizio-9009/restaurants ./restaurants
-docker build --rm -t swarm.inf.uniroma3.it:5000/servizio-9009/zuul ./zuul
+docker build --rm -t swarm.inf.uniroma3.it:5000/eureka-9009 ./eureka
+docker build --rm -t swarm.inf.uniroma3.it:5000/cities-9009 ./cities
+docker build --rm -t swarm.inf.uniroma3.it:5000/ratings-9009 ./ratings
+docker build --rm -t swarm.inf.uniroma3.it:5000/restaurants-9009 ./restaurants
+docker build --rm -t swarm.inf.uniroma3.it:5000/zuul-9009 ./zuul
--- push-all-images.sh
+++ push-all-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker push swarm.inf.uniroma3.it:5000/servizio-9009/eureka
-docker push swarm.inf.uniroma3.it:5000/servizio-9009/cities
-docker push swarm.inf.uniroma3.it:5000/servizio-9009/ratings
-docker push swarm.inf.uniroma3.it:5000/servizio-9009/restaurants
-docker push swarm.inf.uniroma3.it:5000/servizio-9009/zuul
+docker push swarm.inf.uniroma3.it:5000/eureka-9009
+docker push swarm.inf.uniroma3.it:5000/cities-9009
+docker push swarm.inf.uniroma3.it:5000/ratings-9009
+docker push swarm.inf.uniroma3.it:5000/restaurants-9009
+docker push swarm.inf.uniroma3.it:5000/zuul-9009
